90b8c076a4eb7780c1788e8adb8bf7eb113da166
Badrus Salam <Badrus Salam@Assalam>
2016-05-13 16:23:16 +0700

    Bismillah

---
+++ src/main/java/com/mycompany/testmaven/coba.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.testmaven;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Badrus Salam
+ */
+public class coba {
+    String nama;
+    Scanner sc = new Scanner(System.in);
+    
+public static void main(String[] args) {
+      
+        System.out.println("Nama Lengkap : " + nama);
+    }
+}
